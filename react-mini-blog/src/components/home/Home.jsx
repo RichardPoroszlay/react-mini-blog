c96a14d05006b328628b0bdc8ada34d095639813
Richard Poroszlay <richardporoszlay@gmail.com>
2024-12-23 14:37:56 +0100

    Make blogs removable

--- react-mini-blog/src/components/home/Home.jsx
+++ react-mini-blog/src/components/home/Home.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import BlogList from "../bloglist/BlogList";
+
 export default function Home() {
+  const [blogs, setBlogs] = useState([
+    { title: "Title1", body: "some text...", author: "richie", id: 1 },
+    { title: "Title2", body: "some text...", author: "mario", id: 2 },
+    { title: "Title3", body: "some text...", author: "richie", id: 3 },
+  ]);
+
+  function handleDelete(id) {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <h2>Home page</h2>
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
     </div>
   );
 }
