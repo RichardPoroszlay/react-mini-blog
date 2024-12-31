89907e605e38ad1a42e676ddd0305258665d85d2
Richard Poroszlay <richardporoszlay@gmail.com>
2024-12-31 14:24:11 +0100

    Create a NotFound component and do some styling

--- react-mini-blog/src/App.jsx
+++ react-mini-blog/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import NewBlog from "./components/newBlog/NewBlog";
 import BlogDetails from "./components/blogDetails/BlogDetails";
+import NotFound from "./components/notFound/NotFound";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route path="/newblog" element={<NewBlog />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
